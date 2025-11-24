d7206d0db1214166f3cdb766ea4fe0ebf6b3d88d
samia <samia@sprintive@gmail.com>
2025-11-24 17:03:27 +0300

    cleaned the comments

--- createUserDir.php
+++ createUserDir.php
@@ -1,26 +1,4 @@
 <?php
-
-// function createUserdir()
-// {
-//     $username = isset($_SESSION['user']) ? $_SESSION['user'] : null;
-
-//     $base_dir = 'users/';
-//     $user_dir = $base_dir . $username . '/';
-//     if (!is_dir($user_dir)) {
-//         if (mkdir($user_dir, 0777, true)) {
-//             chmod($user_dir, 0777);
-//             header('Location: index.php');
-//             exit();
-//             return true;
-//         } else {
-//             echo "Failed to create directory: " . $user_dir;
-//             header('Location: index.php');
-//             exit();
-//             return false;
-//         }
-//     }
-// }
-
 function createUserdir()
 {
     $username = isset($_SESSION['user']) ? $_SESSION['user'] : null;
--- uploadfile.php
+++ uploadfile.php
@@ -1,33 +1,4 @@
 <?php
-// session_start();
-// $username = isset($_SESSION['user']) ? $_SESSION['user'] : null;
-// // $baseDir = __DIR__;
-// $currentdir = isset($_POST['currentdir']) ? trim($_POST['currentdir'], '/') : '';
-// $absolutePath =  $currentdir;
-// // $currentdir = isset($_POST['currentdir']) ? trim($_POST['currentdir'], '/') : $username;
-
-
-// if (isset($_FILES['uploadedFile'])) {
-
-//     $fileName = $_FILES['uploadedFile']['name'];
-//     $fileType = $_FILES['uploadedFile']['type'];
-//     $fileSize = $_FILES['uploadedFile']['size'];
-//     $fileError = $_FILES['uploadedFile']['error'];
-//     $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
-
-//     if ($fileError == UPLOAD_ERR_OK) {
-//         $dest = $absolutePath . '/' . $fileName;
-
-//         if (move_uploaded_file($fileTmpPath, $dest)) {
-
-//             header("Location: ../index.php?dir=" . urlencode($currentdir));
-//             exit();
-//         }
-//     }
-//     echo "error uploading file<br>";
-//     exit();
-// }
-
 session_start();
 $username = isset($_SESSION['user']) ? $_SESSION['user'] : null;
 $currentdir = rtrim($_POST['currentdir'], '/');
